3ac006ca35d446c259a8d14b833e5ca3ba397c4a
Nigoraaa <94783351+Nigoraaa@users.noreply.github.com>
2021-11-21 18:10:04 +0300

    tree
    tree

--- src/ishchi/js/Tree.js
+++ src/ishchi/js/Tree.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import style from '../css/Tree.module.css'
 
 export default class Tree extends Component {
   render() {
-    return <div>Hello Tree</div>;
+    return <div>
+      <button className={style.btnPlus}>+</button>
+      <h2>334444555 <span>sum</span></h2>
+      <button>-</button>
+    </div>;
   }
 }
