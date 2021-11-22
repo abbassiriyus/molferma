281b2b0866898134191ff1add0290dab6c859a59
Nigoraaa <94783351+Nigoraaa@users.noreply.github.com>
2021-11-22 17:24:56 +0300

    hhjh
    sd

--- src/ishchi/js/Tree.js
+++ src/ishchi/js/Tree.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import style from '../css/Tree.module.css'
 
 export default class Tree extends Component {
   render() {
-    return <div>
-      <button className={style.btnPlus}>+</button>
-      <h2>334444555 <span>sum</span></h2>
-      <button>-</button>
-    </div>;
+    return(
+   <div>
+      <Container>
+      <Row>
+      <Col lg={3}>
+      <button className={style.btnPlus}>+</button></Col>
+      <Col lg={3}><h2>334444555 <span>so'm</span></h2></Col>
+      <Col><button>-</button></Col>
+      </Row>
+    </Container>
+   </div>)
   }
 }
