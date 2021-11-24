a1c03c3f2ba04291fc2cf9317ec05a6679a286f3
Akbarshoh <93857309+intelligento3@users.noreply.github.com>
2021-11-24 21:39:15 +0500

    gfghffhg
    nklnnkln

--- src/ishchi/js/Taxrirlash.js
+++ src/ishchi/js/Taxrirlash.js
@@ -1,10 +1,303 @@
 import React, { Component } from 'react';
+import { Container, Col, Row, Form, Button } from 'react-bootstrap';
 import style from '../css/Taxrirlash.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SizeContext from 'antd/lib/config-provider/SizeContext';
 export default class Taxrirlash extends Component {
     render() {
         return (
-            <div>
-                Salom Taxrirlash 
+            <div className={style.taxrir}>
+                <center><h1>Saytning taxrirlash bolimi</h1></center>
+                <center><h2>Yangi zotlar</h2></center>
+                <div className={style.input}>
+                <input type="button" value="1-chi zot"></input>
+                <input type="button" value="2-chi zot"></input>
+                <input type="button" value="3-chi zot"></input>
+                <input type="button" value="4-chi zot"></input>
+                </div>
+                <center><h2>Foto lavhalar</h2></center>
+   <Container>
+       <Row>
+           <Col lg={4} className={style.form}><Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="img" className={style.but}/>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+</Form>   </Col>
+           <Col lg={4} className={style.form}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="img" className={style.but}/>
+  </Form.Group>
+ <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={4} className={style.form}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="img" className={style.but}/>
+  </Form.Group>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+       </Row>
+   </Container>
+   <Container>
+       <Row>
+           <Col lg={4} className={style.form}><Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+</Form>   </Col>
+           <Col lg={4} className={style.form}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+ <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={4} className={style.form}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+       </Row>
+   </Container>
+   <Container>
+       <Row>
+           <Col lg={4} className={style.form}><Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="img" className={style.but}/>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+</Form>   </Col>
+           <Col lg={4} className={style.form}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="img" className={style.but}/>
+  </Form.Group>
+ <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={4} className={style.form}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv}>Rasm:</Form.Label>
+    <Form.Control type="img" className={style.but}/>
+  </Form.Group>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv}>Tarixi:</Form.Label>
+    <Form.Control type="text" className={style.but}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+       </Row>
+   </Container>
+   
+   
+   
+   <center><h2>Boglanish</h2></center>
+   <Container>
+       <Row>
+           <Col lg={2} className={style.form1}><Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv1}>Bosh offis tel:</Form.Label>
+    <Form.Control type="text" placeholder="+9989********" className={style.but1}/>
+  </Form.Group>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv1}>Bosh offis manzili:</Form.Label>
+    <Form.Control type="text" placeholder="Manzil" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+</Form>   </Col>
+           <Col lg={2} className={style.form1}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label className={style.yozuv1}>Qoshimcha offis tel:</Form.Label>
+    <Form.Control type="text" placeholder="+9989********" className={style.but1}/>
+  </Form.Group>
+ <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv1}>Qoshimcha offis manzili:</Form.Label>
+    <Form.Control type="text" placeholder="Manzil" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form1}>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+<Form.Label className={style.yozuv2}>Boshliq tel:</Form.Label>
+    <Form.Control type="text" placeholder="+9989********" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form1}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv2}>Boshliq orinbosari tel:</Form.Label>
+    <Form.Control type="text" placeholder="+9989********" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form1}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv2}>Ish boshqaruvchi tel:</Form.Label>
+    <Form.Control type="text" placeholder="+9989********" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+       </Row>
+   </Container>
+  
+   
+    <Container>
+       <Row>
+       <Col lg={2} className={style.form2}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv3}>Facebook:</Form.Label>
+    <Form.Control type="text" placeholder="@example" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form2}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv3}>Twitter:</Form.Label>
+    <Form.Control type="text" placeholder="@example" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form2}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv3}>Youtube:</Form.Label>
+    <Form.Control type="text" placeholder="@example" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form2}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv3}>Instagram:</Form.Label>
+    <Form.Control type="text" placeholder="@example" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+           <Col lg={2} className={style.form2}>
+<Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label className={style.yozuv3}>Telegram:</Form.Label>
+    <Form.Control type="text" placeholder="@example" className={style.but1}/>
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Tasdiqlash
+  </Button>
+           </Col>
+       </Row>
+   </Container>
             </div>
         );
     }
